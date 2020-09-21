4c6d606b71ac27317676a857031126c3fbaaeefd
Artur Gunka <arturel101010@mail.ru>
2020-09-21 08:59:42 +0300

    Updates

---
+++ precache-manifest.cda919b86976fe84c89d2928ac1a2acf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134da7d5fd13462f1c4d6c3704634592",
+    "url": "/web-terminal/index.html"
+  },
+  {
+    "revision": "b81456e06c6e0dd5f0d3",
+    "url": "/web-terminal/static/css/main.ba7e0fae.chunk.css"
+  },
+  {
+    "revision": "0ea50baed38b1d1c580e",
+    "url": "/web-terminal/static/js/2.164058d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web-terminal/static/js/2.164058d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81456e06c6e0dd5f0d3",
+    "url": "/web-terminal/static/js/main.d52881a8.chunk.js"
+  },
+  {
+    "revision": "347550a0a973e1e5446b",
+    "url": "/web-terminal/static/js/runtime-main.025202e0.js"
+  }
+]);
